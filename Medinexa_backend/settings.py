3dd4026acd03fb5b05d3b2f592a122ae32171b2f
webmamun25 <webmamun25@gmail.com>
2025-07-28 04:41:44 -0700

    last updated

--- Medinexa_backend/settings.py
+++ Medinexa_backend/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
-
+CSRF_TRUSTED_ORIGINS = ['https://medi-nexa.onrender.com/']
 
 # Application definition
 
